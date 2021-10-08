9ebd5d4a78f5b6bd2db179139bdd1909b01bd113
ipikin <illia.pikin@dgngames.com>
2021-10-08 16:30:53 +0300

    Fixed convert audio stream function

--- app/src/main/java/com/hypnotriod/utils/PcmConvertUtil.java
+++ app/src/main/java/com/hypnotriod/utils/PcmConvertUtil.java
@@ -4,8 +4,8 @@ public class PcmConvertUtil {
     public static double[] convert16BitMono(byte[] data) {
         double[] result = new double[data.length / 2];
         int sample;
-        for (int i = 0; i < result.length; i += 2) {
-            sample = (short) ((data[i + 1] << 8) | (data[i] & 0xFF));
+        for (int i = 0; i < result.length; i++) {
+            sample = (short) (((data[i * 2 + 1] & 0xFF) << 8) | (data[i * 2] & 0xFF));
             result[i] = (double) sample / (double) Short.MAX_VALUE;
         }
         return result;
