b74e4495dbde621e1020942f218df07763cab47e
Ilya Pikin <hypnotiod@gmail.com>
2021-10-13 00:28:09 +0300

    Fixed note name determination algorithm

--- app/src/main/java/com/hypnotriod/chordguesser/dsp/utils/NotesUtil.java
+++ app/src/main/java/com/hypnotriod/chordguesser/dsp/utils/NotesUtil.java
@@ -1,12 +1,12 @@
 package com.hypnotriod.chordguesser.dsp.utils;
 
 public class NotesUtil {
-    public static final String[] NOTES = new String[]{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
+    public static final String[] NOTES = new String[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
     public static final double A4_HZ = 440;
 
     private static double getNoteIndexRaw(double frequency) {
-        return (Math.log(frequency / A4_HZ) / Math.log(2)) * 12;
+        return (Math.log(frequency / A4_HZ) / Math.log(2)) * 12 + 9;
     }
 
     public static String getNoteCentsByFrequency(double frequency) {
@@ -18,13 +18,13 @@ public class NotesUtil {
     public static String getNoteByFrequency(double frequency) {
         int noteIndex = (int) Math.round(getNoteIndexRaw(frequency));
         int octaveNumber = 4;
-        while (noteIndex < 0) {
-            noteIndex += 12;
-            octaveNumber--;
-        }
-        while (noteIndex >= 12) {
-            noteIndex -= 12;
-            octaveNumber++;
+        if (noteIndex < 0) {
+            int octavesNum = noteIndex / -12 + 1;
+            octaveNumber -= octavesNum;
+            noteIndex = (octavesNum * 12 + noteIndex) % 12;
+        } else if (noteIndex >= 12) {
+            octaveNumber += noteIndex / 12;
+            noteIndex %= 12;
         }
         return NOTES[noteIndex] + octaveNumber;
     }
